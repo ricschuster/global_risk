67126982043dc84ebb048bd4c81281ec9db53a1e
Richard Schuster <RichardSchuster@cunet.carleton.ca>
2019-12-10 11:31:46 -0500

    script now accomodates data_resolution variable

--- code/R/analysis/01_example runs.R
+++ code/R/analysis/01_example runs.R
@@ -7,29 +7,30 @@ library(here)
 ## Define functions
 source(here("code/R/functions/multi-objective-prioritization.R"))
 
+data_resolution <- "300km2"
 
-pu <- raster(here("data/intermediate/land.tif"))
-wdpa <- raster(here("data/intermediate/wdpa_terrestrial.tif"))
+pu <- raster(here("data/intermediate/", data_resolution, "land.tif"))
+wdpa <- raster(here("data/intermediate/", data_resolution, "wdpa_terrestrial.tif"))
 locked_in <- ifelse(!is.na(wdpa[][!is.na(pu[])]), TRUE, FALSE)
 
 
-if( !file.exists("data/intermediate/rij.rds")){
+if( !file.exists(paste0("data/intermediate/", data_resolution, "/rij.rds"))){
   
   amph <- stack(list.files(here("data/raw/IUCN/Amph/"), full.names = TRUE))
   rij_amph <- rij_matrix(pu, amph)
-  saveRDS(rij_amph, here("data/intermediate/rij_amph.rds"))
+  saveRDS(rij_amph, here("data/intermediate/", data_resolution, "rij_amph.rds"))
   
   bird <- stack(list.files(here("data/raw/IUCN/Bird/"), full.names = TRUE))
   rij_bird <- rij_matrix(pu, bird)
-  saveRDS(rij_bird, here("data/intermediate/rij_bird.rds"))
+  saveRDS(rij_bird, here("data/intermediate/", data_resolution, "rij_bird.rds"))
   
   mamm <- stack(list.files(here("data/raw/IUCN/Mamm/"), full.names = TRUE))
   rij_mamm <- rij_matrix(pu, mamm)  
-  saveRDS(rij_mamm, here("data/intermediate/rij_mamm.rds"))
+  saveRDS(rij_mamm, here("data/intermediate/", data_resolution, "rij_mamm.rds"))
   
   rept <- stack(list.files(here("data/raw/IUCN/Rept/"), full.names = TRUE))
   rij_rept <- rij_matrix(pu, rept)
-  saveRDS(rij_rept, here("data/intermediate/rij_rept.rds"))
+  saveRDS(rij_rept, here("data/intermediate/", data_resolution, "rij_rept.rds"))
   
   rij <- rbind(rij_amph, rij_bird, rij_mamm, rij_rept)
   
@@ -40,30 +41,30 @@ if( !file.exists("data/intermediate/rij.rds")){
                      name = names(features),
                      stringsAsFactors = FALSE)
   
-  saveRDS(rij, here("data/intermediate/rij.rds"))
-  saveRDS(spec, here("data/intermediate/spec.rds"))
+  saveRDS(rij, here("data/intermediate/", data_resolution, "rij.rds"))
+  saveRDS(spec, here("data/intermediate/", data_resolution, "spec.rds"))
   
 } else {
-  rij <- readRDS("data/intermediate/rij.rds")
-  spec <- readRDS("data/intermediate/spec.rds")
+  rij <- readRDS(here("data/intermediate/", data_resolution, "rij.rds"))
+  spec <- readRDS(here("data/intermediate/", data_resolution, "spec.rds"))
 }
 
 
 ###
- # rij_amph <- readRDS("data/intermediate/rij_amph.rds")
- # rij_bird <- readRDS("data/intermediate/rij_bird.rds")
- # rij_mamm <- readRDS("data/intermediate/rij_mamm.rds")
- # rij_rept <- readRDS("data/intermediate/rij_rept.rds")
+ # rij_amph <- readRDS("data/intermediate/", data_resolution, "rij_amph.rds")
+ # rij_bird <- readRDS("data/intermediate/", data_resolution, "rij_bird.rds")
+ # rij_mamm <- readRDS("data/intermediate/", data_resolution, "rij_mamm.rds")
+ # rij_rept <- readRDS("data/intermediate/", data_resolution, "rij_rept.rds")
 # 
 # rij <- rbind(rij_amph, rij_bird, rij_mamm, rij_rept)
 
 #for testing
  # rij <- rij_amph 
 
-wb_mean <- raster(here("data/intermediate/wb_mean.tif"))
-ssp2 <- raster(here("data/intermediate/ssp2_year_50_threat_score.tif"))
-# clim_grid_ann <- raster(here("data/intermediate/probability-annual-iucn.tif"))
-clim_vel <- raster(here("data/intermediate/climate_vel_garc.tif"))
+wb_mean <- raster(here("data/intermediate/", data_resolution, "wb_mean.tif"))
+ssp2 <- raster(here("data/intermediate/", data_resolution, "ssp2_year_50_threat_score.tif"))
+# clim_grid_ann <- raster(here("data/intermediate/", data_resolution, "probability-annual-iucn.tif"))
+clim_vel <- raster(here("data/intermediate/", data_resolution, "climate_vel_garc.tif"))
 ###
 # only keep values that are present in all 3 threat layers
 ###
